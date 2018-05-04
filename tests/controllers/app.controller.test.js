07bc2511609a47dc29d71ec925ba09a617ebe7d0
yishai <yishai5555@gmail.com>
2018-05-04 17:37:43 +0300

    b06-stub

--- tests/controllers/app.controller.test.js
+++ tests/controllers/app.controller.test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 const sinon = require("sinon");
 const indexPage = require("../../controllers/app.controller.js");
 
-describe("getIndexPage", function () {
+xdescribe("getIndexPage", function () {
     it("should return index page", function () {
         let req = {}
         // Have `res` have a send key with a function value coz we use `res.send()` in our func
@@ -20,7 +20,7 @@ describe("getIndexPage", function () {
     });
 });
 
-describe("getIndexPage", function () {
+xdescribe("getIndexPage", function () {
     it("should send hey", function () {
         let req = {}
         // Have `res` have a send key with a function value coz we use `res.send()` in our func
@@ -57,5 +57,45 @@ describe("User", function () {
             expect(user.addUser.calledOnce).to.be.true;
             // expect(user.addUser.calledTwice).to.be.true;
         });
+    });
+});
+
+
+
+
+
+describe("AppController", function()  {
+    describe("getIndexPage", function() {
+
+        it("should send something else when user is NOT logged in", function() {
+            // instantiate a user object with an empty isLoggedIn function
+            let user = {
+                isLoggedIn: function(){}
+            }
+
+            // Stub isLoggedIn function and make it return false always
+            const isLoggedInStub = sinon.stub(user, "isLoggedIn").returns(false);
+
+            // pass user into the req object
+            let req = {
+                user: user
+            }
+
+            // Have `res` have a send key with a function value coz we use `res.send()` in our func
+            let res = {
+                // replace empty function with a spy
+                send: sinon.spy()
+            }
+
+            indexPage.getIndexPage(req, res);
+            // let's see what we get on res.send
+            // console.log(res.send);
+            // `res.send` called once
+            expect(res.send.calledOnce).to.be.true;
+            expect(res.send.firstCall.args[0]).to.equal("Ooops. You need to log in to access this page");
+
+            // assert that the stub is logged in at least once
+            expect(isLoggedInStub.calledOnce).to.be.true;
+        })
     });
 });
