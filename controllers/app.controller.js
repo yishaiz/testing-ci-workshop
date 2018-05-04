3d91505b0cd03691181d9f3f18024313e487df75
yishai <yishai5555@gmail.com>
2018-05-04 16:46:55 +0300

    b04-spy-pass

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -2,6 +2,7 @@ module.exports = {
     // A func that takes in two parameters `req` and `res` [request, response]
     getIndexPage : (req, res) => {
         console.log('run the getIndexPage function')
-        res.send("Hey");
+        res.send("bla");
+        // res.send("Hey");
     }
 }
