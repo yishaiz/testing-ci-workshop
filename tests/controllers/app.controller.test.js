487acbc68b847396e765568c4f2da1a15b4a0427
yishai <yishai5555@gmail.com>
2018-05-04 16:56:18 +0300

    b05

--- tests/controllers/app.controller.test.js
+++ tests/controllers/app.controller.test.js
@@ -20,13 +20,13 @@ describe("getIndexPage", function () {
     });
 });
 
-describe("getIndexPage", function() {
-    it("should send hey", function() {
+describe("getIndexPage", function () {
+    it("should send hey", function () {
         let req = {}
         // Have `res` have a send key with a function value coz we use `res.send()` in our func
         let res = {
             // replace empty function with a spy
-            send: sinon.spy()
+            send : sinon.spy()
         }
 
         indexPage.getIndexPage(req, res);
@@ -35,6 +35,27 @@ describe("getIndexPage", function() {
         // `res.send` called once
         expect(res.send.calledOnce).to.be.true;
         // expect to get argument `bla` on first call
-        expect(res.send.firstCall.args[0]).to.equal("bla");
+        expect(res.send.firstCall.args[ 0 ]).to.equal("bla");
+    });
+});
+
+
+const user = {
+    addUser: (name) => {
+        this.name = name;
+    }
+}
+
+describe("User", function () {
+    describe("addUser", function () {
+        it("should add a user", function () {
+            sinon.spy(user, "addUser");
+
+            // lets log `addUser` and see what we get
+            console.log(user.addUser());
+
+            expect(user.addUser.calledOnce).to.be.true;
+            // expect(user.addUser.calledTwice).to.be.true;
+        });
     });
 });
